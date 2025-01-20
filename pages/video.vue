b1539f32ae2a04ec6bea13f229b0b61529e7ffa7
ybkim <353007811@qq.com>
2025-01-20 11:23:27 +0800

    邀请二维码

--- pages/video.vue
+++ pages/video.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import {useTitleStore} from "~/store/useDetailLayouts";
+
 definePageMeta({
   layout: 'detail'  // 使用指定的布局
 })
 const videoOptions = {
   autoplay: true,
   sources: [
-    { src: 'https://app.yammevli.cn/cdn/intro.mp4', type: 'video/mp4' }
+    {src: 'https://app.yammevli.cn/cdn/intro.mp4', type: 'video/mp4'}
   ]
 }
 const titleStore = useTitleStore()
@@ -14,10 +15,10 @@ const titleStore = useTitleStore()
 titleStore.setTitle('玩法介绍')
 </script>
 <template>
-<!--  <video-player class="video-player" :options="videoOptions" />-->
+  <!--  <video-player class="video-player" :options="videoOptions" />-->
   <video id="video" class="video-js" controls>
-    <source src="https://app.yammevli.cn/cdn/intro.mp4" type="video/mp4" />
-<!--    <source src="https://path-to-your-video.webm" type="video/webm" />-->
+    <source src="https://app.yammevli.cn/cdn/intro.mp4" type="video/mp4"/>
+    <!--    <source src="https://path-to-your-video.webm" type="video/webm" />-->
   </video>
 
 </template>
@@ -25,7 +26,7 @@ titleStore.setTitle('玩法介绍')
 <style scoped>
 #video {
   width: 100%;
-height: 500px;
+  height: 500px;
   margin: 0 auto;
 }
 </style>
